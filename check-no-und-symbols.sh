e9ceb6732c26c28388ee9f1927fda75c191195e9
Vic Luo <vicluo96@gmail.com>
2018-07-20 23:32:24 +0800

    use cc for link to be more standard-conformant

--- check-no-und-symbols.sh
+++ check-no-und-symbols.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -x
-ld.bfd -o /tmp/a.o build/libd-rlib.a
+cc -nostdlib -nodefaultlibs -lgcc -nostdinc -o /tmp/a.o build/libd-rlib.a
 if nm --undefined-only /tmp/a.o | grep 'U' | grep -v '_start'; then
     echo "found missing symbols..."
     exit 1
