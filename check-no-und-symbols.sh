f831877532cce387942ed4658c777d7a76707698
Vic Luo <vicluo96@gmail.com>
2018-07-20 23:14:06 +0800

    link: use ld.bfd instead of ld
    On some system, ld points to gold, which fails
    silently when called with ld libfoo.a -o foo.
    It seems that there must be at least one .o
    as command line argument.

--- check-no-und-symbols.sh
+++ check-no-und-symbols.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -x
-ld -o /tmp/a.o build/libd-rlib.a
+ld.bfd -o /tmp/a.o build/libd-rlib.a
 if nm --undefined-only /tmp/a.o | grep 'U' | grep -v '_start'; then
     echo "found missing symbols..."
     exit 1
