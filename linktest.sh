e51cbced301864ee38f748d1a6fad62caf5c8c6d
Vic Luo <vicluo96@gmail.com>
2018-07-21 00:18:21 +0800

    fix link error

--- linktest.sh
+++ linktest.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cc -nostdlib -nodefaultlibs -lgcc -nostdinc -o "$DUB_TARGET_PATH/lib$DUB_TARGET_NAME.a" -o "$DUB_TARGET_PATH/$DUB_TARGET_NAME"
+cc -nostdlib -nodefaultlibs -lgcc -nostdinc "$DUB_TARGET_PATH/lib$DUB_TARGET_NAME.a" -o "$DUB_TARGET_PATH/$DUB_TARGET_NAME"
